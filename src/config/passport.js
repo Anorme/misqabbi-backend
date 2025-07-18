e3c41737cf23873c39431c4a7205c1bcbf8ea568
Anorme <anormeinkumsah@gmail.com>
2025-07-18 09:46:08 +0000

    feat(auth): configure passport local strategy for email/password login

---
+++ src/config/passport.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/users.mongo");
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) return done(null, false, { message: "User not found" });
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch)
+          return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
