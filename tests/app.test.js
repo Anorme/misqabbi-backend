f7eaa8ce2056916b6670aeb7eba6cdebc1f9a5c5
Anorme <anormeinkumsah@gmail.com>
2025-07-19 20:42:20 +0000

    test(app): fix supertest usage by removing redundant request(app) wrapper

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-undef */
-import { request } from "supertest";
+import supertest from "supertest";
 import app from "../src/app.js";
+
+const request = supertest(app);
 
 describe("Health Check", () => {
   it("should return 200 OK with a welcome message", async () => {
-    const res = await request(app).get("/");
+    const res = await request.get("/");
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty("message", "Misqabbi backend is live");
   });
