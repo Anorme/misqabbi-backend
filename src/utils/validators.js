1913b4d2514e64d5af36d2ea41674ca75a612723
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-08-12 14:29:01 +0000

    style: run a prettier fix

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -10,4 +10,4 @@ export function isPasswordValidOrGoogleUser(value, doc) {
   return doc.googleId || STRONG_PASSWORD_REGEX.test(value);
 }
 
-export { EMAIL_REGEX, STRONG_PASSWORD_REGEX , OBJECTID_REGEX };
+export { EMAIL_REGEX, STRONG_PASSWORD_REGEX, OBJECTID_REGEX };
