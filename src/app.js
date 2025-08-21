0b6d51fbedc2613011b739a014c647212da66ec7
Anorme <anormeinkumsah@gmail.com>
2025-08-21 17:52:09 +0000

    chore(app): wire swagger into app.js

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import { serve, setup } from "swagger-ui-express";
 
 import "./config/passport.js";
 import corsOptions from "./config/cors.js";
+import swaggerSpec from "./config/swagger.js";
 
 import adminRoutes from "./routes/admin.routes.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -27,6 +29,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors(corsOptions(env.NODE_ENV)));
+app.use("/api-docs", serve, setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Misqabbi backend is live" });
