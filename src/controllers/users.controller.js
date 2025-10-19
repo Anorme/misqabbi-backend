87e7101574a0cef4f2e964b566467626da44e1d3
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-10-19 00:02:43 +0000

    feature: add update user details controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -346,3 +346,35 @@ export const resetPassword = async (req, res) => {
     );
   }
 };
+
+export const updateUserProfile = async (req, res) => {
+  const userId = req.user._id;
+  const { displayName, contact, location } = req.body;
+  try {
+    const user = await findUserById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.displayName = displayName || user.displayName;
+    user.contact = contact || user.contact;
+    user.location = location || user.location;
+
+    user.profileComplete = Boolean(
+      user.displayName && user.contact && user.location
+    );
+
+    await user.save();
+    res.json({
+      message: "Profile updated successfully",
+      user: {
+        displayName: user.displayName,
+        contact: user.contact,
+        location: user.location,
+        profileComplete: user.profileComplete,
+      },
+    });
+  } catch (error) {
+    logger.error(`[updateUserProfile] ${error.message}`);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
