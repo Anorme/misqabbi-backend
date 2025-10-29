313884eeade11d48a27e891340be9c2a0faf7fc4
Anorme <anormeinkumsah@gmail.com>
2025-10-29 12:53:08 +0000

    feat(cloudinary): update Cloudinary configuration and add multer storage for product image uploads

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,10 +1,21 @@
 import env from "./env";
 import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
   cloud_name: env.CLOUDINARY_CLOUD_NAME,
   api_key: env.CLOUDINARY_API_KEY,
-  api_secret: env.CLOUDINARY_API_KEY,
+  api_secret: env.CLOUDINARY_API_SECRET,
+});
+
+export const productUploads = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "misqabbi/products",
+    },
+  }),
 });
 
 export default cloudinary;
