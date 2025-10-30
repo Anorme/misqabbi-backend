49ef43a259e3b0141bd0269cb6b271efa04a9246
Anorme <anormeinkumsah@gmail.com>
2025-10-30 18:52:24 +0000

    feat(users.controller): include user role in current user response

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -172,6 +172,7 @@ export async function getCurrentUser(req, res) {
           contact: user?.contact,
           location: user?.location,
           profileComplete: user?.profileComplete,
+          role: user?.role,
         },
       },
     })
