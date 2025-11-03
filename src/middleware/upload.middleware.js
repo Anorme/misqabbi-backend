2797b91a02f803061a990fa1f3d4c325442ac065
Anorme <anormeinkumsah@gmail.com>
2025-11-03 17:34:29 +0000

    refactor(upload.middleware): simplify image URL generation using cloudinary utility to genereate optimised url

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,13 +1,25 @@
 import logger from "../config/logger.js";
-import { extractPublicIdFromUrl } from "../config/cloudinary.js";
+import cloudinary from "../config/cloudinary.js";
+
+export const getOptimisedUrl = (publicId, options = {}) => {
+  return cloudinary.url(publicId, {
+    width: 800,
+    crop: "limit",
+    fetch_format: "auto",
+    quality: "auto",
+    secure: true,
+    ...options,
+  });
+};
 
 export const attachImagesToBody = (req, res, next) => {
   if (req.files && req.files.length > 0) {
     // Map multer-storage-cloudinary file objects to { url, publicId }
-    req.body.images = req.files.map(file => ({
-      url: file.path,
-      publicId: extractPublicIdFromUrl(file.path),
-    }));
+    req.body.images = req.files.map(file => {
+      const publicId = file.filename;
+      const url = getOptimisedUrl(publicId);
+      return { url, publicId };
+    });
   }
   logger.info(
     `[upload.middleware] Attached ${req.files.length} images to request body`
