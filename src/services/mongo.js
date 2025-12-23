2f3863eaf1ce3f54c584b54f5b0dfba4f77a87df
Anorme <anormeinkumsah@gmail.com>
2025-12-23 19:51:51 +0000

    feat(mongo): update mongoConnect to use MONGO_DB_NAME for database connection

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import logger from "../config/logger.js";
 
 const MONGO_URL = env.MONGO_URL;
+const MONGO_DB_NAME = env.MONGO_DB_NAME;
 
 mongoose.connection.once("open", () => {
   logger.info("Mongoose connection ready!");
@@ -15,7 +16,9 @@ mongoose.connection.on("error", err => {
 });
 
 async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL, {
+    dbName: MONGO_DB_NAME,
+  });
 }
 
 async function mongoDisconnect() {
